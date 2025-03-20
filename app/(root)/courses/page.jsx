579259c64c37ed4f521cb1cbdba48ccacebb0a30
Ashraf Manna <optimisticashraf@gmail.com>
2025-03-21 01:19:45 +0600

    courses page updated

--- app/(root)/courses/page.jsx
+++ app/(root)/courses/page.jsx
@@ -2,10 +2,10 @@
 import React from "react";
 
 export const metadata = {
-  title: "Services",
+  title: "Courses",
   description: "this is services page",
 };
 
-export default function services() {
-  return <div>services</div>;
+export default function Courses() {
+  return <div>Courses to be set</div>;
 }
